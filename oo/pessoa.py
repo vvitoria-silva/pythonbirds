a5d17415a099b633a1e61e5f46d9f8326bfc6d52
Vitória Silva <vvitoriasantossilva@gmail.com>
2021-02-16 17:16:16 -0400

    criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None,idade=19):
+    def __init__(self, *filhos, nome=None, idade=19):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Vitória'
-    print(p.nome)
+    silva = Pessoa(nome='Silva')
+    vitoria = Pessoa(silva, nome='Vitória')
+    print(Pessoa.cumprimentar(vitoria))
+    print(id(vitoria))
+    print(vitoria.cumprimentar())
+    print(vitoria.nome)
+    print(vitoria.idade)
+    for filho in vitoria.filhos:
+        print(filho.nome)
